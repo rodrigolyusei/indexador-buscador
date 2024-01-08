7cb1780f46565d6f75e21de6b79a675efdafb50c
Kevin-Tamayose <125572668+Kevin-Tamayose@users.noreply.github.com>
2024-01-08 20:26:24 -0300

    Update arvore.h

--- arvore.h
+++ arvore.h
@@ -6,7 +6,7 @@ struct Node {
     struct Node* esquerda;
     struct Node* direita;
     int linha;
-} No;
+} Nod;
 
 void addNo(struct Node** raiz, struct Node* no);
 struct Node* Busca(struct Node* raiz, const char* plvr);
