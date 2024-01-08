2f7069cba51a1343981293bf8d66979cbe91c016
Kevin-Tamayose <125572668+Kevin-Tamayose@users.noreply.github.com>
2024-01-08 16:14:49 -0300

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -1,1 +1,26 @@
 #include "arvore.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void addNo(struct Node* raiz, struct Node* no) {
+    if (raiz== NULL) {
+        raiz = no;
+    } else {
+        int i = strcmp(no->palavra, raiz->palavra);
+        if ( i <= 0) {
+            if (raiz->esquerda == NULL) {
+                raiz->esquerda = no;
+            } else {
+                addNo(raiz->esquerda, no);
+//Refaz a função porém comparando com o elemento a esquerda 
+            }}
+       if ( i > 0) {
+            if (raiz->direita == NULL) {
+                raiz->direita = no;
+            } else {
+                addNo(raiz->direita, no);
+            }
+        }        
+    }
+}
